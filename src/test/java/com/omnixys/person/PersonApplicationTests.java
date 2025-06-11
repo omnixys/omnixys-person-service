d5cbf37d8b24664b72574baa7a347aeaaac96730
Caleb-Script <caleb-script@outlook.de>
2025-06-11 14:13:39 +0200

    Update PersonApplicationTests.java

--- src/test/java/com/omnixys/person/PersonApplicationTests.java
+++ src/test/java/com/omnixys/person/PersonApplicationTests.java
@@ -1,12 +1,19 @@
 package com.omnixys.person;
 
+import com.omnixys.person.utils.Env;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest()
 class PersonApplicationTests {
+
+    @BeforeAll
+    protected static void setup() {
+        new Env();
+    }
 
     @Test
     void contextLoads() {
